75837cd114dcaa22a0ba1036525a65cbc0570553
alu0100782851 <alu0100782851@ull.edu.es>
2015-12-11 11:29:15 +0000

    Añadiendo clase hija documentos electronicos

--- lib/prct_10/Doc_electronicos.rb
+++ lib/prct_10/Doc_electronicos.rb
@@ -1,1 +1,19 @@
-
+class Doc_electronicos < Bibliografias
+		
+	attr_accessor :medio, :editor, :url, :acceso
+	
+	def initialize(autores, titulo, publicacion, edicion, medio, editor, url, acceso)
+		
+		super(autores, titulo, publicacion, edicion)
+		
+		@medio = medio
+		@editor = editor
+		@url = url
+		@acceso = acceso
+	end
+	
+	
+	def to_s
+		"#{@autores} (#{@publicacion}). #{@titulo} (#{@edicion}), [#{@medio}]. #{@editor}. Disponible en: #{@url} [#{@acceso}].\n"
+	end
+end
