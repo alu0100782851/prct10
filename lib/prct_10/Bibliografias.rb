ef25c0109635728f4bcad0a8e5bc2294c678e0d0
alu0100782851 <alu0100782851@ull.edu.es>
2015-12-11 11:18:31 +0000

    Añadiendo y modificando metodos para utilizar modulo comparable

--- lib/prct_10/Bibliografias.rb
+++ lib/prct_10/Bibliografias.rb
@@ -13,6 +13,49 @@ class Bibliografias
     end
     
     
+    def <=>(other)
+	
+        return nil unless other.is_a?Bibliografias  #Devuelve nil si se cumple que other no es un objeto de tipo bibliografia
+       
+        if(@autores == other.autores)
+		
+			if(@titulo == other.titulo)
+						return 0 
+			else
+				    arr = [@titulo, other.titulo]
+				    arr.sort_by!{|t| t.downcase}
+					
+					if(arr.first == @titulo)
+						return 1
+					end
+					    return -1
+			end
+
+        else
+
+			arr = [@autores, other.autores]
+			arr.sort_by!{|t| t.downcase}
+			
+			if(arr.first == @autores)
+				return -1
+			end
+    			return 1
+        end
+		
+
+    end
+
+    def ==(other)
+		
+        if other.is_a?Bibliografias
+            @titulo == other.titulo && @autores == other.autores && @publicacion == other.publicacion &&  @edicion == other.edicion
+        else
+            false
+        end
+
+    end
+    
+    
     def APA_autores(other)
         
         string=""
