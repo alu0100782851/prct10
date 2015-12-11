a9c2408da3956ad3573451d3b4a4dac6aa3d98b7
alu0100782851 <alu0100782851@ull.edu.es>
2015-12-11 11:29:50 +0000

    Añadiendo clase hija periodicos

---
+++ lib/prct_10/Periodicos.rb
@@ -0,0 +1,18 @@
+class Periodicos < Bibliografias
+	
+	attr_accessor :nombre, :paginas
+		
+	def initialize(autores, titulo, publicacion, edicion=nil, nombre, paginas)
+			
+		super(autores, titulo, publicacion, edicion)
+			
+        @nombre = nombre
+		@paginas = paginas
+			
+	end
+		
+		
+	def to_s
+		"#{@autores} (#{@publicacion}). #{@titulo}. #{@nombre}, pp. #{@paginas}.\n"		
+	end
+end
